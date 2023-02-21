a15a74ade9664bfa2ffc9743907c23c7b6bcac7f
Nasim <nasimhamdamov2017@mail.ru>
2023-02-21 17:00:41 +0300

    master

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
